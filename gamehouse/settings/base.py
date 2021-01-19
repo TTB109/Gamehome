b51aba585f89afaf84660bcb925f46271ecb908d
Rodríguez Iván <ivan_rodriguez3000@hotmail.com>
2021-01-19 16:29:48 -0600

    aboso

--- gamehouse/settings/base.py
+++ gamehouse/settings/base.py
@@ -98,7 +98,7 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y','%d-%m-%Y') #('%d-%m-%Y','%Y-%m-%d')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,
   ('css','%s/css' % STATIC_DIR),
